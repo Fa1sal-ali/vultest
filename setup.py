57b5ef8776598519998cd4e8dfa2dea5910e156e
Fa1sal-ali <fzl.ali33@gmail.com>
2020-12-08 22:28:38 +0530

    Updated setup.py file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='vultest',
     packages=find_packages(include=['vultest']),
-    version='0.1.1',
+    version='0.1.2',
     description='Vultest is a python library for testing router vulnerability.',
     author='Faisal Ali',
     author_email='fzl.ali33@gmail.com',
