88fc3d6cad4c7d4555bb16c600c1bfe92c748ea6
Fa1sal-ali <fzl.ali33@gmail.com>
2020-12-08 17:14:38 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='vultest-fa1zali',
-    packages=find_packages(),
+    name='vultest',
+    packages=find_packages(include=['vultest']),
     version='0.1.0',
     description='Vultest is a python library for testing router vulnerability.',
     author='Faisal Ali',
     author_email='fzl.ali33@gmail.com',
     url='https://github.com/Fa1sal-ali/vultest',
+    platform='any',
     license='MIT',
-    install_requires=['ipaddress','paramiko','re','time'],
+    install_requires=['ipaddress','paramiko'],
     python_requires='>=3.8'
 )
